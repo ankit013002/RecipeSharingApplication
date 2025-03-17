de3aae742d024e868ded43ce0f9d4f270ddf2c5f
amin-231 <amin.231@osu.edu>
2025-03-16 22:20:49 -0400

    Add Firebase to backend
    Add Firebase to backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("recipesharingapplication-firebase.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+doc_ref = db.collection("users").document("alovelace")
+doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
 
 app = Flask(__name__)
 CORS(app) 
