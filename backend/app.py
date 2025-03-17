cfd23c20b1e695e16531a61550e25f86c249b343
ankit013002 <patel.3854@osu.edu>
2025-03-16 22:30:00 -0400

    Updated location of api access for firebase.

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("recipesharingapplication-firebase.json")
+cred = credentials.Certificate("../recipesharingapplication-firebase.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
